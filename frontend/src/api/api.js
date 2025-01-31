f3c777a43deda6750e82b54439d27818dbe4fbf6
gcordano <gustavohcordano@gmail.com>
2025-01-31 16:01:45 -0300

    add

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// Pegando o valor da API do .env ou usando um padrão caso não esteja definido
+const apiUrl = process.env.REACT_APP_API_URL;
+
+// Criando a instância do Axios com baseURL configurável
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Back-end PHP
+  baseURL: apiUrl,
 });
 
 export default api;
